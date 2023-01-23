edaf69530a9dfd82fdde9057820c679a8dfa83eb
eiki tsutsui <i041ff@yamaguchi-u.ac.jp>
2023-01-24 02:26:42 +0900

    tiny fix

--- cmd/main.go
+++ cmd/main.go
@@ -14,20 +14,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-// 	log.Println(fmt.Sprintf(`
-// ワーカ数: %s,
-// ジョブ数: %s,
-// 参加率: %s,
-// 離脱率: %s,
-// 初期のワーカの参加率: %s,
-// 並列数: %s `,
-// 		os.Getenv("WORKER_MAX_DEFAULT"),
-// 		os.Getenv("JOB_MAX_DEFAULT"),
-// 		os.Getenv("JOINING_RATE_DEFAULT"),
-// 		os.Getenv("DROPOUT_RATE_DEFAULT"),
-// 		os.Getenv("INITIAL_JOINING_RATE_DEFAULT"),
-// 		os.Getenv("LOOP_COUNT_DEFAULT"),
-// 	))
 	workerLimit, err := strconv.Atoi(os.Getenv("WORKER_LIMIT"))
 	if err != nil {
 		log.Fatal("Error loading workerLimit")
