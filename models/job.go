d24c021705e2f11877486f7ad6883769ef0606d6
eiki tsutsui <i041ff@yamaguchi-u.ac.jp>
2023-01-24 03:31:17 +0900

    jobs generics

--- models/job.go
+++ models/job.go
@@ -7,9 +7,18 @@ import (
 type Job struct {
 	ID             int
 	GroupID        int
-	// TODO state になんの種類があるか確認し、int で管理するのをやめてみる
 	State          state.JobState
 	// TODO Worker との二重結合を解消し、親子関係をはっきりさせる
 	AssignedWorker *Worker
 	IsAssigned     bool
+}
+
+func NewJob(id int, groupID int, state state.JobState, assignedWorker *Worker, isAssigned bool) *Job {
+	return &Job{
+		ID: id,
+		GroupID: groupID,
+		State: state,
+		AssignedWorker: assignedWorker,
+		IsAssigned: isAssigned,
+	}
 }
